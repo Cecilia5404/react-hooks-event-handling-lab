398239360758e3170bdd3226befad8cbaa630102
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-07-17 14:20:25 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function eventFocus(){
+        console.log('Good!')
+    }
+    function eventBlur(){
+      console.log('Hey! Eyes on me!')
+  }
+  
+      return(
+  
+          <button onBlur={eventBlur} onFocus={eventFocus}>Eyes on me</button>
+      )
+  }
+  export default EyesOnMe;
